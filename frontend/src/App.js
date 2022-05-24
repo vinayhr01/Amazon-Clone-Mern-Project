a9bf064c44dd2b46cb5d96387d365b8d1bec5c99
VINAY H R <vinayhr1211@gmail.com>
2022-05-24 09:33:22 +0530

    Added admin screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,6 +28,8 @@ import axios from 'axios';
 import SearchBox from './components/SearchBox';
 import SearchScreen from './screens/SearchScreen';
 import ProtectedRoute from './components/ProtectedRoute';
+import DashBoardScreen from './screens/DashBoardScreen';
+import AdminRoute from './components/AdminRoute';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -43,74 +45,91 @@ function App() {
 
   return (
     <BrowserRouter>
-        <ToastContainer position="bottom-center" limit={1}></ToastContainer>
-        <header>
-          <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg" fixed='top' >
-            <Container>
-              <LinkContainer to="/">
-                <Navbar.Brand><NavIcon></NavIcon>{' '}amazonia</Navbar.Brand>
-              </LinkContainer>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-                <SearchBox />
-                <Nav className="me-auto w-100 justify-content-end" collapseOnSelect>
-                  <Link to="/cart" className="nav-link">
-                    Cart
-                    {cart.cartItems.length > 0 && (
-                      <Badge pill bg="danger">
-                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                      </Badge>
-                    )}
-                  </Link>
-                  {UserInfo ? (
-                    <NavDropdown title={UserInfo.name} id="basic-nav-dropdown">
-                      <LinkContainer to="/profile">
-                        <NavDropdown.Item>User Profile</NavDropdown.Item>
-                      </LinkContainer>
-                      <LinkContainer to="/orderhistory">
-                        <NavDropdown.Item>Order History</NavDropdown.Item>
-                      </LinkContainer>
-                      <NavDropdown.Divider />
-                      <Link
-                        className="dropdown-item"
-                        to="#signout"
-                        onClick={SignOutHandler}>
-                        Sign Out
-                      </Link>
-                    </NavDropdown>
-                  ) : (
-                    <Link className="nav-link" to="/signin">
-                      Sign In
-                    </Link>
+      <ToastContainer position="bottom-center" limit={1}></ToastContainer>
+      <header>
+        <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg" fixed='top' >
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand><NavIcon></NavIcon>{' '}amazonia</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <SearchBox />
+              <Nav className="me-auto w-100 justify-content-end" collapseOnSelect>
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
                   )}
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        </header>
-
-        <main>
-          <Container className="mt-3">
-            <Routes>
-              <Route path="/product/:slug" element={<ProductScreen />} />
-              <Route path="/cart" element={<CartScreen />} />
-              <Route path="/signin" element={<SignInScreen />} />
-              <Route path="/search" element={<SearchScreen />} />
-              <Route path="/signup" element={<SignUpScreen />} />
-              <Route path="/profile" element={<ProtectedRoute><ProfileScreen /></ProtectedRoute>} />
-              <Route path='/placeorder' element={<PlaceOrderScreen />} />
-              <Route path="/order/:id" element={<ProtectedRoute><OrderScreen /></ProtectedRoute>} />
-              <Route path="/orderhistory" element={<ProtectedRoute><CartHistoryScreen /></ProtectedRoute>} />
-              <Route path="/shipping" element={<ShippingAddressScreen />} />
-              <Route path='/payment' element={<PaymentScreen />} />
-              <Route path='/' element={<HomeScreen />} />
-            </Routes>
+                </Link>
+                {UserInfo ? (
+                  <NavDropdown title={UserInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="#signout"
+                      onClick={SignOutHandler}>
+                      Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    Sign In
+                  </Link>
+                )}
+                {UserInfo && UserInfo.isAdmin && (
+                  <NavDropdown title="Admin" id="admin-nav-dropdown">
+                    <LinkContainer to="/admin/dashboard">
+                      <NavDropdown.Item>Dashboard</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/admin/productlist">
+                      <NavDropdown.Item>Products</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/admin/orderlist">
+                      <NavDropdown.Item>Orders</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/admin/userlist">
+                      <NavDropdown.Item>Users</NavDropdown.Item>
+                    </LinkContainer>
+                  </NavDropdown>
+                )}
+              </Nav>
+            </Navbar.Collapse>
           </Container>
-        </main>
-        <footer>
-          <div className="text-center">All rights reserved &copy; 2022 </div>
-        </footer>
-    </BrowserRouter>
+        </Navbar>
+      </header>
+
+      <main>
+        <Container className="mt-3">
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/cart" element={<CartScreen />} />
+            <Route path="/signin" element={<SignInScreen />} />
+            <Route path="/search" element={<SearchScreen />} />
+            <Route path="/signup" element={<SignUpScreen />} />
+            <Route path="/profile" element={<ProtectedRoute> <ProfileScreen /></ProtectedRoute>} />
+            <Route path="/placeorder" element={<PlaceOrderScreen />} />
+            <Route path="/order/:id" element={<ProtectedRoute><OrderScreen /></ProtectedRoute>}></Route>
+            <Route path="/orderhistory" element={<ProtectedRoute><CartHistoryScreen /></ProtectedRoute>}></Route>
+            <Route path="/shipping" element={<ShippingAddressScreen />} />
+            <Route path="/payment" element={<PaymentScreen />} />
+            <Route path="/admin/dashboard" element={<AdminRoute> <DashBoardScreen /> </AdminRoute>}></Route>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All rights reserved &copy; 2022 </div>
+      </footer>
+    </BrowserRouter >
   );
 }
 
--- frontend/src/components/AdminRoute.js
+++ frontend/src/components/AdminRoute.js
@@ -2,8 +2,8 @@ import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { Store } from '../Store';
 
-export default function ProtectedRoute({ children }) {
+export default function AdminRoute({ children }) {
     const { state } = useContext(Store);
     const { UserInfo } = state;
-    return UserInfo ? children : <Navigate to="/signin" />;
+    return UserInfo && UserInfo.isAdmin ? children : <Navigate to="/signin" />;
 }
